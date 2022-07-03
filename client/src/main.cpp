263d3326ea14932979454878287ebcb61ef69627
NeronSuper <byblikru228@gmail.com>
2022-07-03 11:38:14 +0200

    -

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,6 +3,12 @@
 #include "cliBaseApp.cpp"
 #include <ctime>
 
+
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+
+
+
 void mainMenu()
 {
 	std::system("clear");
@@ -13,7 +19,26 @@ void mainMenu()
 
 auto main()->int
 {
+
 	
+
+	std::string tempStr;
+    std::string path = "/home/neronsuper/Documents/vsc projects/Messanger/Database/users/";
+    
+ 
+ 
+    // for (auto& p : fs::directory_iterator(inputPath))
+    // {
+    //     path = p;
+	// 	std::cout << path.filename() << "\n";
+    // }
+	
+	for (auto & p : fs::directory_iterator(path)){
+		if (!(p.path().filename() == "NeonSuper"))
+        	std::cout << p.path().filename() << std::endl;
+	}
+
+#if 0
 	//singelton
 	BaseApp *baseApp = BaseApp::instance();
 	//read private data from file
@@ -49,6 +74,7 @@ auto main()->int
 
 	//signIn();
 	//signUp();
-			
+
+#endif
 	return 0;
 }
